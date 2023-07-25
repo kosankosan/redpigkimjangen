948339310b80ba31c18aefbedc0e4d1cfa5f8d61
kosankosan <piaohuizhen@naver.com>
2023-07-25 13:13:17 +0900

    깃허브 처음

---
+++ me.py
@@ -0,0 +1,50 @@
+"""print("ㅈㄷㄱ")
+print("I will not play at tug o' war")
+print("I'd rather play at hug o' war")
+print("where everyone hugs")
+print("instead of tugs,")
+print("where everyonegiggles")
+print("And rolls on the rug,")
+print("where everyone kisses,")
+print("And everyone grins")
+print("And everyone cuddles,")
+print("And everyone wins")
+print("")
+print("")
+print("             -김정은-   ")
+print(234567456123456789098765432123456789098765432123456789098765432123456789078)
+print(12345-12345)
+print(123345678901234567890*45)
+print(23456712345432345432345643234565434565432345643234543234543/12345)
+print(234567//234567)
+print("고")
+print(3+2)
+print(5-4)
+print(3*4)
+print(2%2)
+print(12/3)
+print(123//12)
+print(1234)
+print(234567)
+koone=1
+print(koone)
+ko="금속탐지기"
+san="사주세요"
+print(ko)
+print(san)
+a1=2+2
+b2=2-2
+c3=2*2
+"""
+a=input()
+b="입니다"
+print(a)
+print(b)
+c=input()
+d="를 좋아합니다"
+print(c)
+print(d)
+
+
+
+
