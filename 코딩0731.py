b3d3f47c78d5d23444ec7b54fbbb0138c8fb20fc
kosankosan <piaohuizhen@naver.com>
2023-07-31 13:27:47 +0900

    add

---
+++ 코딩0731.py
@@ -0,0 +1,29 @@
+#print(' " ')
+#print(" ' ")
+#ko=input()
+#san=input()
+#print(ko)
+#print(san)
+#print("바보입니다")
+#print(ko,san,sep="전")
+a="가지볶음"
+print(a*11129)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
