723bed07f7b255f29e1cd71a7b4cbb46218fac43
kosankosan <piaohuizhen@naver.com>
2023-08-02 17:54:54 +0900

    0802코딩

--- 코딩0731.py
+++ 코딩0731.py
@@ -8,15 +8,17 @@
 #print(ko,san,sep="전")
 #a="가지볶음"
 #print(a*11129)
-b="kosanbabo"
-print(b*121)
-
-
-
-
-
-
-
+#b="kosanbabo"
+#print(b*1299991)
+a="돼지"
+b="정은"
+a+=b
+print("빌어먹을 "+a+" 엿먹어라")
+d="친구야 "
+s="화성인 니얼굴  "
+s*=999
+print(d)
+print(s)
 
 
 
