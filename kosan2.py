5b55b2cf2bd1135baa8f0fbc43fa90de67fc6ca2
kosankosan <piaohuizhen@naver.com>
2023-08-08 15:54:27 +0900

    kosan

---
+++ kosan2.py
@@ -0,0 +1,13 @@
+ㅁ="kokoko kosan"
+print(ㅁ[7:12])
+s="kosankosan"
+print(s[::4])
+a="kosankosankosan"
+print(a[::-1])
+z="12121202312"
+x="천인먿나천재농치황"
+ㅊ="kosankosankosankoonekosan"
+print(z[5:9])
+print(x[4:6])
+print(ㅊ[15:20])
+print(f"{z} {x} {ㅊ} 입니다")
