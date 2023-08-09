fe9a8d58829a5e40296c4939ba87053b55eb6952
kosankosan <piaohuizhen@naver.com>
2023-08-09 15:21:07 +0900

    숙제

--- 숙제.py
+++ 숙제.py
@@ -4,5 +4,10 @@ b=a*3
 d=b*2
 print(len(b))
 print(d)"""
-s="k1o2o3n4e5"
-print(s[0]+s[2]+s[4]+s[6]+s[8])
+"""s="k1o2o3n4e5"
+print(s[0]+s[2]+s[4]+s[6]+s[8])"""
+a="고원"
+s="은 "
+d="천재이자 "
+g="복서 "
+print(f"{a}{s}{d}{g}입니다")
