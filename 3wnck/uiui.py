0e7a7c18cbc8dad566bb55dd3071f000f6c7fcf3
kosankosan <piaohuizhen@naver.com>
2023-08-30 16:15:24 +0900

    8030

---
+++ 3wnck/uiui.py
@@ -0,0 +1,12 @@
+#a=input()
+s="123456789101112131415161718192021222324252627282930"
+print(type(1))
+print(type(1.0))
+print(type("1"))
+print(type(s))
+liugy=int(s)
+print(type(liugy))
+print(True and False)
+print(False and False)
+print(True and True)
+print(False and True)
