d25a1f6ed570381a6dd179941560336e1ce0440e
kosankosan <piaohuizhen@naver.com>
2023-09-01 16:13:10 +0900

    kosan

---
+++ 3wnck/yer.py
@@ -0,0 +1,13 @@
+a=1
+del a
+#print(a)
+x =None
+print(x)
+x=4555
+print(x)
+print(False or True)
+print(False or False)
+print(True or True)
+print(True or False)
+print(not True)
+print(not False)
