f471cca8ab780da3a092a392168cede2e1eece91
kosankosan <piaohuizhen@naver.com>
2023-09-04 16:08:13 +0900

    0904

---
+++ 기타/9040coding.py
@@ -0,0 +1,10 @@
+#print(((not True)or True)and( False))
+#not True = False
+#False or True = True
+#True and False =False
+print(32==45 and 32>31)
+print(32==45 or 32>31)
+print(not 32>31)
+print(not 32<=31)
+print(bool("it is False"))
+print(False or True)
